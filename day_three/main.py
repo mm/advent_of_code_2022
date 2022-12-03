5da2de80ec661fc64b1e6bd31cfc93d66bd573ad
Matthew Mascioni <639901+mm@users.noreply.github.com>
2022-12-03 00:35:35 -0500

    .. cleaning up day 3 :p

--- day_three/main.py
+++ day_three/main.py
@@ -7,7 +7,7 @@ for letter in ALPHABET:
     i += 1
     PRIORITIES[letter] = i
 
-def input_lines(input_filename: str) -> Generator[Optional[int], None, None]:
+def input_lines(input_filename: str) -> Generator[Optional[str], None, None]:
     """
     Successively returns strings from the input file. Blank lines return as None.
     """
@@ -39,29 +39,26 @@ def part_one():
 
 def part_two():
     # Build rucksack "groups":
-    rucksack_groups = []
     current_rucksack_group = []
     i = 0
+    priority_sum = 0
     for rucksack_string in input_lines('../inputs/day_three.txt'):
+        current_rucksack_group.append(rucksack_string)
         if i == 2:
-            # If it's the 3rd rucksack we're seeing, append it to the group:
-            current_rucksack_group.append(rucksack_string)
-            rucksack_groups.append(current_rucksack_group)
+            # If it's the 3rd rucksack we're seeing, evaluate the group of rucksacks we have.
+            # Evaluate what's common between the three rucksacks we have in the group:
+            group_one, group_two, group_three = [set(rucksack) for rucksack in current_rucksack_group]
+            three_way_intersection = group_one.intersection(group_two).intersection(group_three)
+            # There should only be one intersection, but hey why not:
+            for intersection in three_way_intersection:
+                priority = PRIORITIES[intersection]
+                priority_sum += priority
+            # Get ready to start building up a list of rucksacks again
             i = 0
             current_rucksack_group = []
         else:
-            current_rucksack_group.append(rucksack_string)
             i += 1
 
-    # Now, work on each group:
-    priority_sum = 0
-    for rucksack_group in rucksack_groups:
-        # What is the common element(s) between the three sacks?
-        group_one, group_two, group_three = [set(rucksack) for rucksack in rucksack_group]
-        three_way_intersection = group_one.intersection(group_two).intersection(group_three)
-        for intersection in three_way_intersection:
-            priority = PRIORITIES[intersection]
-            priority_sum += priority
     print(priority_sum)
 
 part_one()
